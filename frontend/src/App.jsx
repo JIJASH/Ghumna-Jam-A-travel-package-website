81d9f6921636a1701b4ff17a03d295655c33afa6
biplavdotcom <lostking05@gmail.com>
2024-12-13 22:27:28 +0545

    frontend files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
+import React from 'react'
 
 const App = () => {
   return (
-    <div className="text-red-700 text-5xl ">App</div>
+    <div className='text-3xl text-red-950 text-center'>App</div>
   )
 }
 
